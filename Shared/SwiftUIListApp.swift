72fee76a4add0e0afc18d3cd697ca4d6146431c3
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-24 19:25:59 +0200

    Initial Commit

---
+++ Shared/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  Shared
+//
+//  Created by Alex Nagy on 24.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
